fcebf1950e379886393a7a98250b137d4ddd629b
Deddoom <amir2020bo4@gmail.com>
2026-08-29 14:17:16 +0330

    feat: configure API routes with Swagger documentation and update nginx server name to wildcard

--- project/urls.py
+++ project/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/', include('core.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui-alias'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
